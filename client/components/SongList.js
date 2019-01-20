ac23beba1bf3359400f2b8d81f4d825156c36e92
dragosbus <dragosbusuioc94@gmail.com>
2019-01-20 12:41:53 +0100

    added react router

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo'
+
+const query = gql `
+	{
+		songs {
+			id
+			title
+		}
+	}
+`;
 
 class SongList extends Component {
-	render() {
-		return (
-			<div></div>
-		);
-	}
+    renderSongs() {
+        return this.props.data.songs.map(song => (
+            <li key={song.id} className="collection-item">
+							{song.title}
+						</li>
+        ))
+    }
+    render() {
+        const {loading} = this.props.data
+        if (loading) return <div>Loading</div>
+        const { songs } = this.props.data
+        return (
+            <ul className="collection">{this.renderSongs()}</ul>
+        );
+    }
 }
 
-export default SongList
+export default graphql(query)(SongList)
