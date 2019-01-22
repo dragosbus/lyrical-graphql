ba76ec13432135a98f53485ef6739fc2289ec67c
dragosbus <dragosbusuioc94@gmail.com>
2019-01-22 07:02:07 +0100

    starting add song mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -6,13 +6,19 @@ class SongCreate extends Component {
         this.state = {
             title: ''
         }
+        this.onSubmit = this.onSubmit.bind(this)
     }
+
+    onSubmit(event) {
+        event.preventDefault()
+    }
+
     render() {
         const { title } = this.state
         return (
             <div>
 							<h3>Create a new song</h3>
-							<form>
+							<form onSubmit={this.onSubmit}>
 								<label>Song title:</label>
 								<input value={title} onChange={event => this.setState({title: event.target.value})}/>
 							</form>
