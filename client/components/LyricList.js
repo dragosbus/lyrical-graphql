7add75441d3d89b6970c2365013c7d2e038111a5
dragosbus <dragosbusuioc94@gmail.com>
2019-01-27 12:36:45 +0100

    handled optimistic response

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -3,10 +3,18 @@ import { graphql } from 'react-apollo'
 import {likeMutation} from '../queries/mutations.js'
 
 class LyricList extends Component {
-	onLike(id) {
+	onLike(id, likes) {
 		this.props.mutate({
 			variables: {
 				id
+			},
+			optimisticResponse: {
+				__typeName: 'Mutation',
+				likeLyric: {
+					id,
+					__typeName: 'LyricType',
+					likes: likes + 1,
+				}
 			}
 		})
 	}
@@ -16,7 +24,7 @@ class LyricList extends Component {
 				{
 					this.props.lyrics.map(lyric => (
 						<li key={lyric.id} style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-							<i onClick={() => this.onLike(lyric.id)} className="material-icons">thumb_up</i>
+							<i onClick={() => this.onLike(lyric.id, lyric.likes)} className="material-icons">thumb_up</i>
 							<p>{lyric.content}</p>
 							<p>{lyric.likes}</p>
 						</li>
