bb91efac27e1905bc2a0a7f66ed1e18eb9bff938
dragosbus <dragosbusuioc94@gmail.com>
2019-01-27 11:09:36 +0100

    passed variables

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
-const SongById = gql`
+const SongById = gql `
 	query songById($id: ID) {
 		song(id: $id) {
 			id
@@ -17,34 +17,32 @@ const SongById = gql`
 `
 
 export class SongDetail extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			data: null,
-		}
-	}
-	componentDidMount() {
-		const {match, data} = this.props
-		console.log(data)
-		if(data.loading) this.setState({data: 'loading'})
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: null,
+        }
+    }
+    componentDidMount() {
+        const { match, data } = this.props
+        console.log(data)
+        if (data.loading) this.setState({ data: 'loading' })
 
-			console.log(this.props)
-	}
-	render() {
-		const {data} = this.state
-		if(data === 'loading') return <div>Loading</div>
-		return (
-			<div>
+        console.log(this.props)
+    }
+    render() {
+        const { data } = this.state
+        if (data === 'loading') return <div>Loading</div>
+        return (
+            <div>
 				<h3>Song</h3>
 			</div>
-		);
-	}
+        );
+    }
 }
 
-const options = props => ({
-	variables: {
-		id: props.id
-	}
-})
-
-export default graphql(SongById, options)(SongDetail)
+export default graphql(SongById, {
+    options: props => ({
+        variables: { id: props.params.id }
+    })
+})(SongDetail)
